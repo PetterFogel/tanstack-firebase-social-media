59d74f6a6a2dff6c5ffd9fe0e42c4a088be9da7e
PetterFogel <petterfogel96@gmail.com>
2024-06-03 18:21:17 +0200

    Install and configure shadcn-ui component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "./components/ui/button";
+import "./global.css";
+
 const App = () => {
   return (
     <div>
-      <h1>Hello, World</h1>
+      <h1 className="text-5xl text-orange-400">Hello, World</h1>
+      <Button onClick={() => console.log("Click")}>Click</Button>
     </div>
   );
 };
