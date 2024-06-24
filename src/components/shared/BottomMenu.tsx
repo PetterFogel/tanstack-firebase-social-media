8f877c79c0abc78ee6c5c5286ab4b90605020b7c
PetterFogel <petterfogel96@gmail.com>
2024-06-24 21:21:06 +0200

    Refactor menu brn styling

--- src/components/shared/BottomMenu.tsx
+++ src/components/shared/BottomMenu.tsx
@@ -18,17 +18,15 @@ const BottomMenu = () => {
   const { pathname } = useLocation();
 
   return (
-    <section className="z-50 border-t flex-between bg-white w-full sticky bottom-0 p-3 md:hidden">
+    <section className="z-50 border-t flex-between bg-white w-full sticky bottom-0 p-4 md:hidden">
       {bottomMenuLinks.map((link, idx) => {
         const isActive = pathname === link.route;
         return (
           <Link
             key={idx}
             to={link.route}
-            className={`rounded-lg text-xs p-3 transition group flex-center flex-col ${
-              isActive
-                ? "bg-slate-900 text-white hover:text-white hover:bg-slate-800"
-                : "hover:bg-gray-100 hover:text-slate-900"
+            className={`rounded-lg text-xs transition group flex-center flex-col ${
+              isActive ? "text-slate-900 font-bold" : "text-slate-600"
             }`}
           >
             {link.svgIcon}
